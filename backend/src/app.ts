c8e44fa250a36cd6f8a5673d56803870c71cce45
Marcos Visentini <marcosvisentini@gmail.com>
2022-02-13 22:25:33 -0300

    Upgrade API documentation using Swagger UI

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,24 +1,25 @@
 import 'reflect-metadata';
 import express from 'express';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 
 import './api/database/database';
 import { env } from './api/config/config';
 import { routes } from './api/routes/routes';
+
+import swaggerDocument from './swagger.json';
 
 const app = express();
 const port = env.app.port;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
   res.sendFile('index.html', { root: 'public/home' });
-})
-
-app.get('/api', (req, res) => {
-  res.sendFile('index.html', { root: 'public/api' });
 })
 
 app.use('/api', routes);
